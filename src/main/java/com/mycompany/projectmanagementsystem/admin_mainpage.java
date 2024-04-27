6323fe775ca0e911d5855c11212807aaeb9f990c
Jasonshe0509 <jasonshe80@gmail.com>
2024-04-27 13:34:41 +0800

    fix: admin student

---
+++ src/main/java/com/mycompany/projectmanagementsystem/admin_mainpage.java
@@ -0,0 +1,188 @@
+package com.mycompany.projectmanagementsystem;
+
+import java.awt.Color;
+import java.awt.Toolkit;
+
+public class admin_mainpage extends javax.swing.JFrame {
+    
+    public admin_mainpage() {
+        initComponents();
+        setIconImage();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        admin_header = new javax.swing.JPanel();
+        admin_logo = new javax.swing.JLabel();
+        admin_lecturer = new javax.swing.JLabel();
+        admin_student = new javax.swing.JLabel();
+        admin_report = new javax.swing.JLabel();
+        admin_profile = new javax.swing.JLabel();
+        admin_logout = new javax.swing.JLabel();
+        admin_greeting = new javax.swing.JLabel();
+        admin_background = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Admin: Main Page");
+        setBackground(new java.awt.Color(255, 255, 255));
+        setMinimumSize(new java.awt.Dimension(1000, 700));
+        setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        admin_header.setBackground(new Color(255, 255, 255, 90));
+        admin_header.setToolTipText("");
+        admin_header.setMaximumSize(new java.awt.Dimension(1000, 73));
+        admin_header.setMinimumSize(new java.awt.Dimension(1000, 73));
+        admin_header.setPreferredSize(new java.awt.Dimension(1000, 73));
+
+        admin_logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Sysco_logo.png"))); // NOI18N
+        admin_logo.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+        admin_lecturer.setBackground(new Color(255, 255, 255, 0));
+        admin_lecturer.setFont(new java.awt.Font("Bell MT", 1, 18)); // NOI18N
+        admin_lecturer.setForeground(new java.awt.Color(2, 50, 99));
+        admin_lecturer.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        admin_lecturer.setText("Lecturer");
+        admin_lecturer.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        admin_lecturer.setMaximumSize(new java.awt.Dimension(96, 73));
+        admin_lecturer.setMinimumSize(new java.awt.Dimension(96, 73));
+        admin_lecturer.setPreferredSize(new java.awt.Dimension(96, 73));
+
+        admin_student.setBackground(new Color(255, 255, 255, 0));
+        admin_student.setFont(new java.awt.Font("Bell MT", 1, 18)); // NOI18N
+        admin_student.setForeground(new java.awt.Color(2, 50, 99));
+        admin_student.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        admin_student.setText("Student");
+        admin_student.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        admin_student.setMaximumSize(new java.awt.Dimension(96, 73));
+        admin_student.setMinimumSize(new java.awt.Dimension(96, 73));
+        admin_student.setPreferredSize(new java.awt.Dimension(96, 73));
+        admin_student.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                admin_studentMouseClicked(evt);
+            }
+        });
+
+        admin_report.setBackground(new Color(255, 255, 255, 0));
+        admin_report.setFont(new java.awt.Font("Bell MT", 1, 18)); // NOI18N
+        admin_report.setForeground(new java.awt.Color(2, 50, 99));
+        admin_report.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        admin_report.setText("Report");
+        admin_report.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        admin_report.setMaximumSize(new java.awt.Dimension(96, 73));
+        admin_report.setMinimumSize(new java.awt.Dimension(96, 73));
+        admin_report.setPreferredSize(new java.awt.Dimension(96, 73));
+
+        admin_profile.setBackground(new Color(255, 255, 255, 0));
+        admin_profile.setFont(new java.awt.Font("Bell MT", 1, 18)); // NOI18N
+        admin_profile.setForeground(new java.awt.Color(2, 50, 99));
+        admin_profile.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        admin_profile.setText("Profile");
+        admin_profile.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        admin_profile.setMaximumSize(new java.awt.Dimension(96, 73));
+        admin_profile.setMinimumSize(new java.awt.Dimension(96, 73));
+        admin_profile.setPreferredSize(new java.awt.Dimension(96, 73));
+
+        admin_logout.setBackground(new Color(255, 255, 255, 0));
+        admin_logout.setFont(new java.awt.Font("Bell MT", 1, 18)); // NOI18N
+        admin_logout.setForeground(new java.awt.Color(2, 50, 99));
+        admin_logout.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        admin_logout.setText("Log Out");
+        admin_logout.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        admin_logout.setMaximumSize(new java.awt.Dimension(96, 73));
+        admin_logout.setMinimumSize(new java.awt.Dimension(96, 73));
+        admin_logout.setPreferredSize(new java.awt.Dimension(96, 73));
+
+        javax.swing.GroupLayout admin_headerLayout = new javax.swing.GroupLayout(admin_header);
+        admin_header.setLayout(admin_headerLayout);
+        admin_headerLayout.setHorizontalGroup(
+            admin_headerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(admin_headerLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(admin_logo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 300, Short.MAX_VALUE)
+                .addComponent(admin_lecturer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(admin_student, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(admin_report, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(admin_profile, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(admin_logout, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        admin_headerLayout.setVerticalGroup(
+            admin_headerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(admin_headerLayout.createSequentialGroup()
+                .addGroup(admin_headerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(admin_headerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(admin_report, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(admin_profile, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(admin_logout, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, admin_headerLayout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addGroup(admin_headerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(admin_student, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(admin_lecturer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap())
+            .addGroup(admin_headerLayout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addComponent(admin_logo)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        getContentPane().add(admin_header, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        admin_greeting.setFont(new java.awt.Font("SansSerif", 0, 18)); // NOI18N
+        admin_greeting.setForeground(new java.awt.Color(2, 50, 99));
+        admin_greeting.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        admin_greeting.setText("Good Day Admin!");
+        admin_greeting.setMaximumSize(new java.awt.Dimension(230, 18));
+        admin_greeting.setMinimumSize(new java.awt.Dimension(230, 18));
+        admin_greeting.setPreferredSize(new java.awt.Dimension(230, 18));
+        getContentPane().add(admin_greeting, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 80, 170, 30));
+
+        admin_background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/main_background.png"))); // NOI18N
+        admin_background.setText("Good Day Admin!");
+        admin_background.setMaximumSize(new java.awt.Dimension(1000, 700));
+        admin_background.setMinimumSize(new java.awt.Dimension(1000, 700));
+        admin_background.setPreferredSize(new java.awt.Dimension(1000, 700));
+        getContentPane().add(admin_background, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void admin_studentMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_admin_studentMouseClicked
+        // TODO add your handling code here:
+        admin_student admin_stu = new admin_student();
+        admin_stu.show();
+        hide();
+    }//GEN-LAST:event_admin_studentMouseClicked
+
+   
+    public static void main(String args[]) {
+        
+        java.awt.EventQueue.invokeLater(() -> {
+            new admin_mainpage().setVisible(true);
+        });
+    }
+    private void setIconImage() {
+        setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Sysco_icon_with_background.png")));
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel admin_background;
+    private javax.swing.JLabel admin_greeting;
+    private javax.swing.JPanel admin_header;
+    private javax.swing.JLabel admin_lecturer;
+    private javax.swing.JLabel admin_logo;
+    private javax.swing.JLabel admin_logout;
+    private javax.swing.JLabel admin_profile;
+    private javax.swing.JLabel admin_report;
+    private javax.swing.JLabel admin_student;
+    // End of variables declaration//GEN-END:variables
+}
