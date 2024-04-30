0ed60062c1e582657aab3835d7db6c0a25604fde
Jasonshe0509 <jasonshe80@gmail.com>
2024-04-30 21:16:52 +0800

    fix: layout issue

--- src/main/java/com/mycompany/projectmanagementsystem/LecturerPresentationFeedback.java
+++ src/main/java/com/mycompany/projectmanagementsystem/LecturerPresentationFeedback.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.projectmanagementsystem;
 
+import java.awt.Toolkit;
+
 /**
  *
  * @author user
@@ -15,6 +17,7 @@ public class LecturerPresentationFeedback extends javax.swing.JFrame {
      */
     public LecturerPresentationFeedback() {
         initComponents();
+        setIconImage();
     }
 
     /**
@@ -46,8 +49,10 @@ public class LecturerPresentationFeedback extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Lecturer Presentation Feedback");
         setMaximumSize(new java.awt.Dimension(700, 500));
         setMinimumSize(new java.awt.Dimension(700, 500));
+        setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel7.setFont(new java.awt.Font("Bell MT", 1, 20)); // NOI18N
@@ -281,6 +286,7 @@ public class LecturerPresentationFeedback extends javax.swing.JFrame {
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 700, 500));
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
@@ -324,6 +330,9 @@ public class LecturerPresentationFeedback extends javax.swing.JFrame {
                 new LecturerPresentationFeedback().setVisible(true);
             }
         });
+    }
+    private void setIconImage() {
+        setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Sysco_icon_with_background.png")));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
