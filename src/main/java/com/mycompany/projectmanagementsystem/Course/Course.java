faefee5f2d9207d4f4fddb069b925199e98f5197
Jasonshe0509 <jasonshe80@gmail.com>
2024-05-13 13:01:50 +0800

    feat: course modal, controller and validator

---
+++ src/main/java/com/mycompany/projectmanagementsystem/Course/Course.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projectmanagementsystem.Course;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Course {
+    private String courseCode;
+    private String courseName;
+
+    public Course(String courseCode, String courseName) {
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+    
+}
