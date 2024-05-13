00758f6e36a27cc819bb7be1d9692434d3ca91f8
Jasonshe0509 <jasonshe80@gmail.com>
2024-05-13 13:48:32 +0800

    fix: intake modal

--- src/main/java/com/mycompany/projectmanagementsystem/Intake/Intake.java
+++ src/main/java/com/mycompany/projectmanagementsystem/Intake/Intake.java
@@ -15,4 +15,63 @@ public class Intake {
     private String courseCode;
     private String enrollYear;
     private String enrollMonth;
+
+    public Intake(String intakeCode, String schoolWiseName, String levelCode, String courseCode, String enrollYear, String enrollMonth) {
+        this.intakeCode = intakeCode;
+        this.schoolWiseName = schoolWiseName;
+        this.levelCode = levelCode;
+        this.courseCode = courseCode;
+        this.enrollYear = enrollYear;
+        this.enrollMonth = enrollMonth;
+    }
+
+    public String getIntakeCode() {
+        return intakeCode;
+    }
+
+    public void setIntakeCode(String intakeCode) {
+        this.intakeCode = intakeCode;
+    }
+
+    public String getSchoolWiseName() {
+        return schoolWiseName;
+    }
+
+    public void setSchoolWiseName(String schoolWiseName) {
+        this.schoolWiseName = schoolWiseName;
+    }
+
+    public String getLevelCode() {
+        return levelCode;
+    }
+
+    public void setLevelCode(String levelCode) {
+        this.levelCode = levelCode;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getEnrollYear() {
+        return enrollYear;
+    }
+
+    public void setEnrollYear(String enrollYear) {
+        this.enrollYear = enrollYear;
+    }
+
+    public String getEnrollMonth() {
+        return enrollMonth;
+    }
+
+    public void setEnrollMonth(String enrollMonth) {
+        this.enrollMonth = enrollMonth;
+    }
+    
+    
 }
