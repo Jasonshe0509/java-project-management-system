2026f80397a3c831cc39a5ffeda2c439bbe087e7
Jasonshe0509 <jasonshe80@gmail.com>
2024-05-13 13:47:13 +0800

    feat: intake modal, controller, and Validator

---
+++ src/main/java/com/mycompany/projectmanagementsystem/Intake/Intake.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projectmanagementsystem.Intake;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Intake {
+    private String intakeCode;
+    private String schoolWiseName;
+    private String levelCode;
+    private String courseCode;
+    private String enrollYear;
+    private String enrollMonth;
+}
