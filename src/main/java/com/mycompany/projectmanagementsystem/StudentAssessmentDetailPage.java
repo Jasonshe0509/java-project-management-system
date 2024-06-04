411fb324b63709a9d8b6301dc5dea08a0656588e
Jasonshe0509 <jasonshe80@gmail.com>
2024-06-04 15:08:30 +0800

    fix: student page

--- src/main/java/com/mycompany/projectmanagementsystem/StudentAssessmentDetailPage.java
+++ src/main/java/com/mycompany/projectmanagementsystem/StudentAssessmentDetailPage.java
@@ -221,8 +221,8 @@ public class StudentAssessmentDetailPage extends javax.swing.JFrame {
                 .addGap(43, 43, 43)
                 .addComponent(jLabel8)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(assessmentStatus, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(399, 399, 399))
+                .addComponent(assessmentStatus, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(360, 360, 360))
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -1224,6 +1224,10 @@ public class StudentAssessmentDetailPage extends javax.swing.JFrame {
                         assessmentStatus.setText("Submitted");
                         assessmentStatus.setBackground(new Color(0x1B3D60)); // Set the background color
                     }
+                    case "partially marked" -> {
+                        assessmentStatus.setText("Partially Marked");
+                        assessmentStatus.setBackground(new Color(0x1B6047)); // Set the background color
+                    }
                     case "marked" -> {
                         assessmentStatus.setText("Marked");
                         assessmentStatus.setBackground(new Color(0x1B6047)); // Set the background color
@@ -1452,7 +1456,13 @@ public class StudentAssessmentDetailPage extends javax.swing.JFrame {
                 String secondMarkerId = assessmentList[5].trim(); // Second Marker ID
                 String[] names = getSupervisorAndSecondMarkerNames(supervisorId, secondMarkerId);
                 supervisorName.setText(names[0]);
-                secondMarkerName.setText(names[1]);
+                if (secondMarkerId.isEmpty()) {
+                    secondMarkerName.setText("-");
+
+                } else {
+                    secondMarkerName.setText(names[1]);
+
+                }
             }
         }
     }
@@ -1796,13 +1806,16 @@ public class StudentAssessmentDetailPage extends javax.swing.JFrame {
                     names[0],
                     "supervisor"
                 };
-                String[] secondMarkerRecord = {
-                    list[5],
-                    names[1],
-                    "second marker"
-                };
                 model.addRow(supevisorRecord);
-                model.addRow(secondMarkerRecord);
+
+                if (names[1] != null) {
+                    String[] secondMarkerRecord = {
+                        list[5],
+                        names[1],
+                        "second marker"
+                    };
+                    model.addRow(secondMarkerRecord);
+                }
                 break;
             }
         }
